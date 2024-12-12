60f220d1c2b4e32503eea767b3111be471a446ca
Tachibana Shin <tachibshin@duck.com>
2024-12-13 00:53:58 +0700

    Add SSL certificate handling and Android compatibility fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/services.dart';
 import 'package:honyomi/background_services/book_changes.dart';
 import 'package:honyomi/plugins/objectbox.dart';
 import 'package:honyomi/router/index.dart';
@@ -10,6 +12,8 @@ import 'package:flutter/material.dart';
 import 'package:go_transitions/go_transitions.dart';
 
 Future<void> main() async {
+  // ca
+  await _installCert();
   // init store
   await initObjectBox();
 
@@ -18,8 +22,35 @@ Future<void> main() async {
   BookChanges().checkUpdateAll();
 
   // register background service
-  if (Platform.isAndroid || Platform.isIOS) {
-    BookChanges().initializeBackgroundService();
+  // if (Platform.isAndroid || Platform.isIOS) {
+  //   BookChanges().initializeBackgroundService();
+  // }
+}
+
+class _MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
+Future<void> _installCert() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  ByteData data =
+      await PlatformAssetBundle().load('assets/ca/lets-encrypt-r3.pem');
+  SecurityContext.defaultContext
+      .setTrustedCertificatesBytes(data.buffer.asUint8List());
+
+  if (Platform.isAndroid) {
+    final deviceInfo = DeviceInfoPlugin();
+    final androidInfo = await deviceInfo.androidInfo;
+
+    if (androidInfo.version.sdkInt < 25) {
+      HttpOverrides.global = _MyHttpOverrides();
+    }
   }
 }
 
