7cd2a7968172c81a955369dff8745ef92dccf837
Tachibana Shin <tachibshin@duck.com>
2025-01-06 02:17:07 +0700

    fix: remove unused _onCanPlay variable in PlayerEiga

--- lib/screens/home_eiga/player_eiga.dart
+++ lib/screens/home_eiga/player_eiga.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hls_parser/flutter_hls_parser.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
-import 'package:get/get.dart';
 import 'package:hoyomi/core_services/eiga/interfaces/source_content.dart';
 import 'package:hoyomi/core_services/interfaces/basic_image.dart';
 import 'package:hoyomi/widgets/eiga/slider_eiga.dart';
