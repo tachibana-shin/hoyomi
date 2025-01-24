df9d82ebe1855759adbcde18fd6d806afcc45fdb
Tachibana Shin <shin@shin.is-a.dev>
2025-01-24 19:51:37 +0700

    Add JSON serialization to BasicUser class

--- lib/core_services/interfaces/basic_user.dart
+++ lib/core_services/interfaces/basic_user.dart
@@ -5,12 +5,33 @@ class BasicUser {
   final String fullName;
   final Sex sex;
 
-  BasicUser(
-      {required this.user,
-      required this.email,
-      required this.photoUrl,
-      required this.fullName,
-      this.sex = Sex.other});
+  BasicUser({
+    required this.user,
+    required this.email,
+    required this.photoUrl,
+    required this.fullName,
+    this.sex = Sex.other,
+  });
+
+  factory BasicUser.fromJson(Map<String, dynamic> json) {
+    return BasicUser(
+      user: json['user'],
+      email: json['email'],
+      photoUrl: json['photoUrl'],
+      fullName: json['fullName'],
+      sex: Sex.values.firstWhere((e) => e.toString() == 'Sex.${json['sex']}'),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'user': user,
+      'email': email,
+      'photoUrl': photoUrl,
+      'fullName': fullName,
+      'sex': sex.toString().split('.').last,
+    };
+  }
 }
 
 enum Sex {
