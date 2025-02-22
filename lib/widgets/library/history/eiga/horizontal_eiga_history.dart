8d1d20d192bdcd9cb20acdb301d7f7b8911aa3cd
Tachibana Shin <shin@shin.is-a.dev>
2025-02-22 17:42:43 +0700

    feat: add vertical layout support for eiga history list

--- lib/widgets/library/history/eiga/horizontal_eiga_history.dart
+++ lib/widgets/library/history/eiga/horizontal_eiga_history.dart
@@ -10,9 +10,15 @@ import 'package:mediaquery_sizer/mediaquery_sizer.dart';
 class HorizontalEigaHistory extends StatefulWidget {
   final String sourceId;
   final HistoryItem<Eiga> history;
+  final double? width;
+  final Axis direction;
 
   const HorizontalEigaHistory(
-      {super.key, required this.sourceId, required this.history});
+      {super.key,
+      required this.sourceId,
+      required this.history,
+      this.width,
+      this.direction = Axis.horizontal});
 
   @override
   State<HorizontalEigaHistory> createState() => _HorizontalEigaHistoryState();
@@ -25,106 +31,110 @@ class _HorizontalEigaHistoryState extends State<HorizontalEigaHistory> {
     final episode = widget.history.lastEpisode;
     final watchTime = widget.history.watchTime;
 
+    final poster = Container(
+        width: widget.width ?? min(180.0, 30.w(context)),
+        decoration: BoxDecoration(
+            color: Colors.blueGrey.shade200,
+            borderRadius: BorderRadius.circular(10.0)),
+        clipBehavior: Clip.antiAlias,
+        child: Stack(children: [
+          AspectRatio(
+              aspectRatio: 16 / 9,
+              child: OImage.network(
+                eiga.image.src,
+                sourceId: widget.sourceId,
+                headers: eiga.image.headers,
+                fit: BoxFit.cover,
+              )),
+
+          ///
+          Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Column(children: [
+                LinearProgressIndicator(
+                  value: watchTime.position.inMilliseconds /
+                      watchTime.duration.inMilliseconds,
+                  backgroundColor: Color.fromRGBO(255, 255, 255, 0.6),
+                  valueColor: AlwaysStoppedAnimation<Color>(Color(0xFF00C234)),
+                  minHeight: 3.0,
+                  borderRadius: BorderRadius.circular(10.0),
+                )
+              ])),
+        ]));
+    final titleAndSubtitle = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 3.0,
+        ),
+        Text(
+          eiga.name,
+          style: Theme.of(context)
+              .textTheme
+              .titleMedium
+              ?.copyWith(fontSize: 16.0, fontWeight: FontWeight.w400),
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+        ),
+
+        Text(
+          'Episode ${episode.name}',
+          style: Theme.of(context).textTheme.titleMedium?.copyWith(
+              fontSize: 14.0,
+              fontWeight: FontWeight.w400,
+              color: Theme.of(context)
+                  .textTheme
+                  .titleMedium
+                  ?.color
+                  ?.withValues(alpha: 0.8)),
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+        ),
+
+        SizedBox(height: 2.0),
+
+        ///
+        Text(
+          '${formatDuration(watchTime.position)} / ${formatDuration(watchTime.duration)}',
+          style: Theme.of(context).textTheme.titleMedium?.copyWith(
+              fontSize: 12.0,
+              fontWeight: FontWeight.w400,
+              color: Theme.of(context)
+                  .textTheme
+                  .titleMedium
+                  ?.color
+                  ?.withValues(alpha: 0.8)),
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+        ),
+        SizedBox(height: 5.0),
+        if (episode.description?.isNotEmpty == true)
+          Text(
+            episode.description!,
+            style:
+                Theme.of(context).textTheme.bodySmall?.copyWith(fontSize: 12.0),
+            maxLines: 2,
+          )
+      ],
+    );
+
     return InkWell(
         borderRadius: BorderRadius.circular(7),
         child: Container(
             decoration: BoxDecoration(
                 color: null, borderRadius: BorderRadius.circular(7.0)),
             padding: EdgeInsets.symmetric(vertical: 7.0, horizontal: 7.0),
-            child: Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Container(
-                  width: min(180.0, 30.w(context)),
-                  decoration: BoxDecoration(
-                      color: Colors.blueGrey.shade200,
-                      borderRadius: BorderRadius.circular(10.0)),
-                  clipBehavior: Clip.antiAlias,
-                  child: Stack(children: [
-                    AspectRatio(
-                        aspectRatio: 16 / 9,
-                        child: OImage.network(
-                          eiga.image.src,
-                          sourceId: widget.sourceId,
-                          headers: eiga.image.headers,
-                          fit: BoxFit.cover,
-                        )),
-
-                    ///
-                    Positioned(
-                        bottom: 0,
-                        left: 0,
-                        right: 0,
-                        child: Column(children: [
-                          LinearProgressIndicator(
-                            value: watchTime.position.inMilliseconds /
-                                watchTime.duration.inMilliseconds,
-                            backgroundColor: Color.fromRGBO(255, 255, 255, 0.6),
-                            valueColor: AlwaysStoppedAnimation<Color>(
-                                Color(0xFF00C234)),
-                            minHeight: 3.0,
-                            borderRadius: BorderRadius.circular(10.0),
-                          )
-                        ])),
-                  ])),
-              SizedBox(width: 7.0),
-              Expanded(
-                  child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 3.0,
-                  ),
-                  Text(
-                    eiga.name,
-                    style: Theme.of(context)
-                        .textTheme
-                        .titleMedium
-                        ?.copyWith(fontSize: 16.0, fontWeight: FontWeight.w400),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-
-                  Text(
-                    'Episode ${episode.name}',
-                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.w400,
-                        color: Theme.of(context)
-                            .textTheme
-                            .titleMedium
-                            ?.color
-                            ?.withValues(alpha: 0.8)),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-
-                  SizedBox(height: 2.0),
-
-                  ///
-                  Text(
-                    '${formatDuration(watchTime.position)} / ${formatDuration(watchTime.duration)}',
-                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                        fontSize: 12.0,
-                        fontWeight: FontWeight.w400,
-                        color: Theme.of(context)
-                            .textTheme
-                            .titleMedium
-                            ?.color
-                            ?.withValues(alpha: 0.8)),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  SizedBox(height: 5.0),
-                  if (episode.description?.isNotEmpty == true)
-                    Text(
-                      episode.description!,
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodySmall
-                          ?.copyWith(fontSize: 12.0),
-                      maxLines: 2,
-                    )
-                ],
-              ))
-            ])));
+            child: widget.direction == Axis.horizontal
+                ? Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                    poster,
+                    SizedBox(width: 7.0),
+                    Expanded(child: titleAndSubtitle)
+                  ])
+                : Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [poster, titleAndSubtitle],
+                  )));
   }
 }
--- lib/widgets/library/history/eiga/horizontal_eiga_history_list.dart
+++ lib/widgets/library/history/eiga/horizontal_eiga_history_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hoyomi/core_services/eiga/interfaces/eiga.dart';
 import 'package:hoyomi/core_services/eiga/mixin/eiga_history_mixin.dart';
 import 'package:hoyomi/core_services/interfaces/history_item.dart';
@@ -7,8 +8,8 @@ import 'package:hoyomi/core_services/main.dart';
 import 'package:hoyomi/core_services/utils_service.dart';
 import 'package:hoyomi/utils/format_watch_update_at.dart';
 import 'package:hoyomi/widgets/eiga/vertical_eiga.dart';
-import 'package:hoyomi/widgets/horizontal_list.dart';
 import 'package:hoyomi/widgets/library/history/eiga/horizontal_eiga_history.dart';
+import 'package:mediaquery_sizer/mediaquery_sizer.dart';
 
 class HorizontalEigaHistoryList extends StatefulWidget {
   final String sourceId;
@@ -34,13 +35,45 @@ class _HorizontalEigaHistoryState extends State<HorizontalEigaHistoryList> {
     super.initState();
   }
 
+  Widget _buildContainer(BuildContext context,
+      {required bool needSubtitle,
+      required Widget Function(double viewFraction) builder}) {
+    return LayoutBuilder(builder: (context, constraints) {
+      double screenWidth = constraints.biggest.width;
+      double crossAxisCount;
+
+      if (screenWidth <= 600) {
+        crossAxisCount = 3.5;
+      } else if (screenWidth <= 900) {
+        crossAxisCount = 4.5;
+      } else {
+        crossAxisCount = 6.5;
+      }
+
+      final childAspectRatio = 16 / 9;
+      final viewportFraction = 1 / crossAxisCount;
+      final height =
+          1 / childAspectRatio * (screenWidth * viewportFraction - 8.0) +
+              7.0 +
+              3.0 +
+              16.0 * 2 +
+              14.0 * 2 +
+              2.0 +
+              12.0 * 2 +
+              5.0 +
+              12.0 * 2;
+
+      return SizedBox(height: height, child: builder(viewportFraction));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
         future: _historyFuture,
         builder: (context, snapshot) {
           if (snapshot.hasError) {
-            return HorizontalList.buildContainer(
+            return _buildContainer(
               context,
               builder: (viewFraction) => Center(
                   child: UtilsService.errorWidgetBuilder(context,
@@ -55,22 +88,59 @@ class _HorizontalEigaHistoryState extends State<HorizontalEigaHistoryList> {
                   30, (_) => HistoryItem.createFakeData(Eiga.createFakeData()))
               : snapshot.data!;
 
-          return HorizontalList<HistoryItem<Eiga>>(
-            title: 'History',
-            subtitle: data.isEmpty
-                ? ''
-                : formatWatchUpdatedAt(data.first.watchUpdatedAt, null),
-            more: '/library/history/eiga/${widget.sourceId}',
-            items: data,
-            needSubtitle: data.firstWhereOrNull(
-                    (eiga) => VerticalEiga.checkNeedSubtitle(eiga.item)) !=
-                null,
-            builder: (context, history, index) {
-              return HorizontalEigaHistory(
-                sourceId: widget.sourceId,
-                history: history,
-              );
-            },
+          final title = 'History';
+          final subtitle = data.isEmpty
+              ? ''
+              : formatWatchUpdatedAt(data.first.watchUpdatedAt, null);
+          final more = '/library/history/eiga/${widget.sourceId}';
+          final items = data;
+          final needSubtitle = data.firstWhereOrNull(
+                  (eiga) => VerticalEiga.checkNeedSubtitle(eiga.item)) !=
+              null;
+
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w600,
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+                  ),
+                  Text(
+                    subtitle,
+                    style: TextStyle(fontSize: 14, color: Colors.white70),
+                  ),
+                ]),
+                ElevatedButton(
+                    onPressed: () {
+                      context.push(more);
+                    },
+                    child: Text('More'))
+              ]),
+              _buildContainer(
+                context,
+                needSubtitle: needSubtitle,
+                builder: (viewportFraction) => PageView.builder(
+                    itemCount: items.length,
+                    allowImplicitScrolling: true,
+                    padEnds: false,
+                    controller: PageController(
+                      viewportFraction: viewportFraction,
+                      initialPage: 0,
+                    ),
+                    itemBuilder: (context, index) => HorizontalEigaHistory(
+                          sourceId: widget.sourceId,
+                          history: items.elementAt(index),
+                          width: 100.w(context) / 1 / viewportFraction,
+                          direction: Axis.vertical,
+                        )),
+              )
+            ],
           );
         });
   }
