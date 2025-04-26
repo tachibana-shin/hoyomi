5fe420a5aa0acb5b07e33d8cae2115919eb9d21f
Tachibana Shin <tachibshin@duck.com>
2025-04-26 07:44:07 +0000

    feat: add route config for web and adjust UI in manga reader
    - Add `_routes.json` for web route configuration.
    - Constrain the width of the reader to a maximum of 900 in manga_reader.dart.
    - Add `alignAtTopCenter()` modifier on the reader builder.
    - Remove unnecessary line break in truyenqq service.

--- lib/screens/home_comic/manga_reader.dart
+++ lib/screens/home_comic/manga_reader.dart
@@ -621,7 +621,10 @@ class _MangaReaderState extends State<MangaReader>
             rows: 3,
             columns: 3,
             onTap: _onTapGrid,
-            child: _buildReader(),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(maxWidth: 900),
+              child: _buildReader(),
+            ).alignAtTopCenter(),
           ),
           Positioned(left: 0, right: 0, bottom: 0, child: _buildTinyStatus()),
           Positioned(left: 0, right: 0, bottom: 0, child: _buildBottomBar()),
