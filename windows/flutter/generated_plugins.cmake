9cbb84b9d33508f26ed4c35bee2c428ee338182e
Tachibana Shin <tachibshin@duck.com>
2025-05-10 01:20:16 +0700

    chore: add sqlite3_flutter_libs plugin support across platforms

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -13,6 +13,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   screen_brightness_windows
   screen_retriever_windows
   share_plus
+  sqlite3_flutter_libs
   url_launcher_windows
   volume_controller
   window_manager
