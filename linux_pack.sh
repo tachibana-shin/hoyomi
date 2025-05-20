e43acae479e8015810022d0491ae1948590146a5
Tachibana Shin <tachibshin@duck.com>
2025-05-20 21:27:37 +0700

    Build linux (#36)
    * Add linux_setup.sh for setting up development environment
    
    * Add flag yes for linux_setup.sh
    
    * add build to github actions

---
+++ linux_pack.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+PUBSPEC="pubspec.yaml"
+APP_NAME=$(grep "^name:" $PUBSPEC | sed 's/name:[ ]*//')
+VERSION=$(grep "^version:" $PUBSPEC | sed 's/version:[ ]*//' | cut -d '+' -f1)
+DESCRIPTION=$(grep "^description:" $PUBSPEC | sed 's/description:[ ]*//')
+
+ARCH="amd64"
+OUT_DIR="deb_output"
+INSTALL_PREFIX="usr/share/$APP_NAME"
+
+rm -rf $OUT_DIR
+mkdir -p $OUT_DIR/DEBIAN
+mkdir -p $OUT_DIR/$INSTALL_PREFIX
+
+cp -r build/linux/x64/release/bundle/* $OUT_DIR/$INSTALL_PREFIX/
+cp app.png $OUT_DIR/$INSTALL_PREFIX/
+
+cat > $OUT_DIR/DEBIAN/control <<EOF
+Package: $APP_NAME
+Version: $VERSION
+Architecture: $ARCH
+Depends: libgtk-3-0, libasound2, libmpv1
+Maintainer: Tachibana Shin <tachibshin@duck.com>
+Description: $DESCRIPTION
+EOF
+
+chmod +x $OUT_DIR/$INSTALL_PREFIX/$APP_NAME || true
+
+mkdir -p $OUT_DIR/usr/share/applications
+cat > $OUT_DIR/usr/share/applications/${APP_NAME}.desktop <<EOF
+[Desktop Entry]
+Name=$APP_NAME
+Exec=/usr/share/$APP_NAME/$APP_NAME
+Icon=/usr/share/$APP_NAME/app.png
+Type=Application
+Categories=Utility;
+EOF
+
+dpkg-deb --build $OUT_DIR app-linux-${ARCH}-release.deb
---
+++ linux_setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin bash
+
+sudo apt update -y
+sudo apt install ninja-build build-essential cmake pkg-config libgtk-3-dev liblzma-dev libasound2-dev libmpv-dev pkg-config -y
