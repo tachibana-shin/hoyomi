30863cd8e26d1f91c6cd0baf4523bf127166c538
Tachibana Shin <shin@shin.is-a.dev>
2025-05-20 13:51:22 +0000

    Add flag yes for linux_setup.sh

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin bash
 
-sudo apt install ninja-build build-essential cmake pkg-config libgtk-3-dev liblzma-dev libasound2-dev libmpv-dev pkg-config
+sudo apt update -y
+sudo apt install ninja-build build-essential cmake pkg-config libgtk-3-dev liblzma-dev libasound2-dev libmpv-dev pkg-config -y
