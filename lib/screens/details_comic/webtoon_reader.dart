e5a0064beda92b442525b808143d625acd9759d8
Tachibana Shin <tachibshin@duck.com>
2025-05-25 16:01:09 +0700

    feat: integrate jk_fast_listview for improved scrolling performance in WebToonReader

--- lib/screens/details_comic/webtoon_reader.dart
+++ lib/screens/details_comic/webtoon_reader.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hoyomi/screens/home_comic/manga_reader.dart';
+import 'package:jk_fast_listview/jk_fast_listview.dart';
 import 'package:kaeru/kaeru.dart';
-import 'package:mediaquery_sizer/mediaquery_sizer.dart';
+// import 'package:mediaquery_sizer/mediaquery_sizer.dart';
 
 class WebToonReader extends StatefulWidget {
   // final int itemCount;
@@ -27,7 +28,7 @@ class _WebToonReaderState extends State<WebToonReader>
   @override
   bool get wantKeepAlive => true;
 
-  final _scrollController = ScrollController();
+  late final _scrollController = ref(JkItemController());
 
   final Map<int, GlobalKey> _itemKeys = {};
   int? _lastReportedPage;
@@ -39,62 +40,65 @@ class _WebToonReaderState extends State<WebToonReader>
   @override
   void initState() {
     super.initState();
-    _scrollController.addListener(_onScroll);
+    // _scrollController.addListener(_onScroll);
 
     watch([widget.pages], () => _itemKeys.clear());
 
     watch([widget.currentPage], () async {
       final pageIndex = widget.currentPage.value.toInt();
-      int i = 0;
-      while (i++ < 30) {
-        if (_lastReportedPage != pageIndex) {
-          if (!mounted) break;
-          _scrollToPage(pageIndex);
-          await Future.delayed(Duration(milliseconds: 100));
-          _onScroll();
-          continue;
-        }
+      // int i = 0;
+      // while (i++ < 30) {
+      // if (_lastReportedPage != pageIndex) {
+      //     if (!mounted) break;
+      //     _scrollToPage(pageIndex);
+      //     await Future.delayed(Duration(milliseconds: 100));
+      //     _onScroll();
+      //     continue;
+      //   }
 
-        break;
+      //   break;
+      // }
+      if (_lastReportedPage != pageIndex) {
+        _scrollController.value = JkItemController(initialIndex: pageIndex);
       }
     }, immediate: true);
   }
 
-  void _onScroll() {
-    for (var entry in _itemKeys.entries) {
-      final ctx = entry.value.currentContext;
-      if (ctx == null) continue;
+  // void _onScroll() {
+  //   for (var entry in _itemKeys.entries) {
+  //     final ctx = entry.value.currentContext;
+  //     if (ctx == null) continue;
 
-      final box = ctx.findRenderObject();
-      if (box is! RenderBox) continue;
+  //     final box = ctx.findRenderObject();
+  //     if (box is! RenderBox) continue;
 
-      final offset = box.localToGlobal(Offset.zero);
+  //     final offset = box.localToGlobal(Offset.zero);
 
-      if (offset.dy >= 0 && offset.dy < MediaQuery.of(context).size.height) {
-        widget.currentPage.value = (_lastReportedPage = entry.key).toDouble();
-        break;
-      }
-    }
-  }
+  //     if (offset.dy >= 0 && offset.dy < MediaQuery.of(context).size.height) {
+  //       widget.currentPage.value = (_lastReportedPage = entry.key).toDouble();
+  //       break;
+  //     }
+  //   }
+  // }
 
-  void _scrollToPage(int index) async {
-    double bef = 0;
-    int befIndex = -1;
-    for (int i = 0; i <= index; i++) {
-      final RenderBox? box =
-          _getKey(i).currentContext?.findRenderObject() as RenderBox?;
-      if (box != null) {
-        bef += box.size.height;
-        befIndex++;
-      }
-    }
+  // void _scrollToPage(int index) async {
+  //   double bef = 0;
+  //   int befIndex = -1;
+  //   for (int i = 0; i <= index; i++) {
+  //     final RenderBox? box =
+  //         _getKey(i).currentContext?.findRenderObject() as RenderBox?;
+  //     if (box != null) {
+  //       bef += box.size.height;
+  //       befIndex++;
+  //     }
+  //   }
 
-    _scrollController.animateTo(
-      100.h(context) * (index - (befIndex + 1)) + bef,
-      duration: Duration(milliseconds: 10),
-      curve: Curves.linear,
-    );
-  }
+  //   _scrollController.animateTo(
+  //     100.h(context) * (index - (befIndex + 1)) + bef,
+  //     duration: Duration(milliseconds: 10),
+  //     curve: Curves.linear,
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -103,10 +107,15 @@ class _WebToonReaderState extends State<WebToonReader>
     return Watch(() {
       return InteractiveViewer(
         minScale: 0.5,
-        child: ListView.builder(
-          controller: _scrollController,
+        child: JkFastListView(
+          key: ValueKey(_scrollController.value),
+          itemController: _scrollController.value,
+          onScrollPosition: (index, _) {
+            widget.currentPage.value = (_lastReportedPage = index).toDouble();
+          },
           itemCount: widget.pages.value.length,
           cacheExtent: 3,
+          addRepaintBoundaries: false,
           itemBuilder: (context, index) {
             final key = widget.pages.value.elementAt(index).image.src;
             return Container(
