8367f355ec2e8a2ed0d0fb0c8815a6cb367a8600
tachibana-shin <tachibshin@duck.com>
2025-06-21 03:33:21 +0000

    typo

--- hoyomi_bridge_ts/register_plugin.ts
+++ hoyomi_bridge_ts/register_plugin.ts
@@ -1,6 +1,6 @@
 import type { BaseService } from "../lib/core_services/main"
 
-export function registerPlugin(plugin: BaseService) {
+export function registerPlugin(plugin: typeof BaseService) {
   Object.assign(globalThis, {
     __$HOYOMI_PLUGIN$__: plugin
   })
