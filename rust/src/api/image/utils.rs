34ddd788e44179b0c86d25bcbd1d892160b7d901
tachibana-shin <tachibshin@duck.com>
2025-06-26 08:46:48 +0000

    ??? rollback rust utils

--- rust/src/api/image/utils.rs
+++ rust/src/api/image/utils.rs
@@ -1,7 +1,5 @@
 use image::{DynamicImage};
 
-use image::{DynamicImage, ImageError};
-
-pub(crate) fn load_image(image: Vec<u8>) -> Result<DynamicImage, ImageError> {
-    image::load_from_memory(image.as_slice())
+pub(crate) fn load_image(image: Vec<u8>) -> DynamicImage {
+    return image::load_from_memory(image.as_slice()).expect("Failed to load image");
 }
